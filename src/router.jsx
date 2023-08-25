d454f26e036c6af0a331ff3d948caafdb82c0355
TeresitaFraszczak <110676528+TeresitaFraszczak@users.noreply.github.com>
2023-08-25 09:39:58 -0300

    Inicio Sprint3

--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom"
 import HeaderLayout from "./layouts/HeaderLayout"
 import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 import Cities from "./pages/Cities"
 import CityDetails from "./pages/CityDetails"
 import NotFound from "./pages/NotFound"
+import Itineraries from "./pages/Itineraries"
+import FormSignUp from "./pages/FormSignUp"
+import FormSignIn from "./pages/FormSignIn"
 
 
 const router = createBrowserRouter([
@@ -14,8 +19,13 @@ const router = createBrowserRouter([
         { path: "/", element: <Home /> },
         { path: "/index", element: <Home />},
         { path: "/home", element: <Home />},
+        { path: "/signin", element: <SignIn />},
+        { path: "/signup", element: <SignUp />},
         { path: "/cities", element: <Cities /> },        
         { path: "/city/:city_id", element: <CityDetails />},
+        { path: "/auth/signup", element: <FormSignUp /> },
+        { path: "/auth/signin", element: <FormSignIn /> },
+        { path: "/itineraries", element: <Itineraries /> },
         { path: "/*", element: <NotFound />},
     ],
   },
